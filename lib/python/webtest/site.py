e7573a4806e816303068b912ec1d4ab4ea79ea5d
Dónal McMullan <donal.mcmullan@gmail.com>
2016-02-11 20:53:23 -0500

    Better docs. Removed debug logging. Renamed some stuff.

--- lib/python/webtest/site.py
+++ lib/python/webtest/site.py
@@ -7,8 +7,10 @@ from webtest import log
 
 logger = log.get_logger()
 
-def eb(f, label="EB2"):
-    logger.error("%s: %s", label, f.getBriefTraceback())
+
+def logging_errback(f, label="logging_errback"):
+    logger.error("%s: %s" % (label, f.getBriefTraceback()))
+
 
 class RedisRequest(Request):
     def session_callback(self, session, cookiename):
@@ -17,27 +19,24 @@ class RedisRequest(Request):
         self.session.touch()
         return self.session
 
-    def igetSession(self, sessionInterface=None):
+    def getSession(self, sessionInterface=None):
         # Session management
-        logger.error("getSession")
         if not self.session:
             cookiename = b"_".join([b'TWISTED_SESSION'] + self.sitepath)
             sessionCookie = self.getCookie(cookiename)
-            logger.error("X1: %s" % (sessionCookie,))
             if sessionCookie:
                 try:
                     df = self.site.getSession(sessionCookie)
                     df.addCallback(self.session_callback, cookiename)
-                    df.addErrback(eb, "get  session")
+                    df.addErrback(logging_errback, "RedisRequest.site.getSession")
                     return df
                 except KeyError:
                     pass
-            logger.error("X2")
             # if it still hasn't been set, fix it up.
             if not self.session:
                 df = self.site.makeSession()
                 df.addCallback(self.session_callback, cookiename)
-                df.addErrback(eb, "make session")
+                df.addErrback(logging_errback, "RedisRequest.site.makeSession")
                 return df
 
         #if sessionInterface:
@@ -50,13 +49,9 @@ class RedisSite(Site):
 
     def makeSession(self):
         """
-        Generate a new Session instance, and store it for future reference.
+        Generate a new Session instance
         """
         uid = self._mkuid()
-        logger.error("makeSession: %s" % (uid,))
-        """
-        Should we even keep sessions in self.sessions?
-        """
         return self.sessionFactory.retrieve(uid, reactor=self._reactor)
 
     def getSession(self, uid):
@@ -64,11 +59,6 @@ class RedisSite(Site):
         Get a previously generated session, by its unique ID.
         This raises a KeyError if the session is not found.
         """
-        """
-        Here we should run out to persistent session storage
-        to see if there's data there
-        """
-        logger.error("site.getSession")
         return self.sessionFactory.retrieve(uid, reactor=self._reactor)
 
 
