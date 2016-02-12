311ed7bb0b629575c0cedc50f8d62a80c22100dc
Dónal McMullan <donal.mcmullan@gmail.com>
2016-02-11 20:54:38 -0500

    Removed dev logging and fix naming of igetSession to getSession

--- lib/python/webtest/web/index.py
+++ lib/python/webtest/web/index.py
@@ -12,13 +12,11 @@ logger = log.get_logger()
 class Root(resource.Resource):
 
     def _render_GET(self, request):
-        logger.error("render_GET")
         session = request.getSession()
-        logger.error("SESSION: %s" % (sorted(dir(session))),)
         return '<html><body>Hello World</body></html>'
 
     def render_GET(self, request):
-        df = request.igetSession()
+        df = request.getSession()
         df.addCallback(self.cb, request)
         df.addErrback(self.eb)
         return server.NOT_DONE_YET
@@ -28,19 +26,15 @@ class Root(resource.Resource):
         someval2 = session.get("SOMEVAL2", -1)
         session['SOMEVAL2'] = someval2 + 1
 
-        logger.error("C: session: %s" % (session,))
-        logger.error("C: someval: %s" % (someval,))
-
         request.write("<html>Hello, world 2!: %s  '%s' %s</html>" % (session.uid, session, request.path,))
         session['SOMEVAL'] = "HELP"
         request.finish()
 
     def eb(self, f):
-        logger.error("index.Root: %s", f.getBriefTraceback())
+        logger.error("ERROR: index.Root: %s", f.getBriefTraceback())
         return f
 
     def getChild(self, name, request):
-        logger.error("getChild: %s path:%s  uri:%s   responseHeaders:%s" % (name, request.path, request.uri, request.responseHeaders,))
         return self
 
 
