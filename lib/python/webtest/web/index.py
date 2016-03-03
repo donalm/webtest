6bdfadf06bb7b62f7ee7e53b9b2de79467394e75
Dónal McMullan <donal.mcmullan@gmail.com>
2016-03-03 01:25:47 -0500

    Unsatisfactory handling of 'finish called on a request after its connection was lost'

--- lib/python/webtest/web/index.py
+++ lib/python/webtest/web/index.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import os
+import exceptions
 from twisted.web import resource
 from twisted.web import server
 
@@ -20,13 +21,17 @@ class Root(resource.Resource):
         return server.NOT_DONE_YET
 
     def cb(self, session, request):
-        someval = session.get("SOMEVAL", 'xxx')
-        someval2 = session.get("SOMEVAL2", -1)
-        session['SOMEVAL2'] = someval2 + 1
+        someval = session.get("SOMEVAL2", -1)
+        session['SOMEVAL2'] = someval + 1
 
-        request.write("<html>%05d Hello, world 2!: %s  '%s' %s</html>" % (os.getpid(), session.uid, session, request.path,))
-        session['SOMEVAL'] = "HELP"
-        request.finish()
+        request.write("<html><pre>         PID: %05d\n Session UID: %s\nSession data: %s\nRequest path: %s</html>" % (os.getpid(), session.uid, session, request.path,))
+        try:
+            request.finish()
+        except exceptions.RuntimeError, e:
+            if e.message[0:64] == '''Request.finish called on a request after its connection was lost''':
+                return
+            logger.error(e)
+            raise
 
     def eb(self, f):
         logger.error("ERROR: index.Root: %s", f.getBriefTraceback())
