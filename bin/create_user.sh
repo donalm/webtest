f5b8f5865fe0ce3a16ccd8cbf0d2e914f8492444
Dónal McMullan <donal.mcmullan@gmail.com>
2016-06-05 22:32:58 -0400

    Add the txwebtest user to the redis group. Add the current user to the txwebtest group

--- bin/create_user.sh
+++ bin/create_user.sh
@@ -1,2 +1,13 @@
-#!/bin/bash
+#!/usr/bin/zsh
 useradd -r -s /bin/false txwebtest
+usermod -a -G redis txwebtest
+
+if [ "$USER" = "root" ]; then
+    user=$SUDO_USER
+else
+    user=$USER
+fi
+
+if [ -n "$user" ]; then
+	usermod -a -G txwebtest $user
+fi
